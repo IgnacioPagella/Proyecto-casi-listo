2726fcc3dfd7297789065d3cd9b71f9e2a1270dc
IgnacioPagella <nacho.pagella@gmail.com>
2022-10-28 12:07:16 -0300

    update

--- Assets/Scrpts/LobbyNetworkManager.cs
+++ Assets/Scrpts/LobbyNetworkManager.cs
@@ -5,7 +5,7 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 
-public class Lobby : MonoBehaviourPunCallbacks
+public class LobbyNetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private InputField _roomInput;
     private void Start()
@@ -18,6 +18,13 @@ public class Lobby : MonoBehaviourPunCallbacks
         PhotonNetwork.NickName = "Player" + Random.Range(0, 5000);
         PhotonNetwork.ConnectUsingSettings();
         PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    #region PhotonCallBacks
+
+    public override OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateRoomList(roomList);
     }
 
     public override void OnConnectedToMaster()
@@ -34,5 +41,23 @@ public class Lobby : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log("Joined Lobby!");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room:" + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void JoinRoom (string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(_roomInput.text) == false)
+        {
+            PhotonNetwork.CreateRoom(_roomInput.text, new RoomOptions() { MaxPlayers = 4 }, null);
+        }
     }
 }
